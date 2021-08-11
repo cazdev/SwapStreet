527ac2f231706d7930082b2279a5a8d564300ed9
Joshua Archer <joshua.archer@students.mq.edu.au>
2021-08-11 12:56:54 +1000

    Added a backend containing a simple GET (users) connecting to the database

---
+++ server/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express') 
+const cors = require('cors')
+const fs = require("fs") 
+const apiRouter = require("./controllers/api")
+
+const app = express() 
+app.use(cors())
+app.use(express.json()) 
+app.use(express.static('build'))
+app.use(apiRouter)
+
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
