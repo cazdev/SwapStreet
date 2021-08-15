0f0088e9c6897c257fd88e8e0ad6154edf62800a
JoshDeeNc <61564763+JoshDeeNc@users.noreply.github.com>
2021-08-15 14:43:06 +1000

    fixed navbar styling

--- src/components/HeadingBar.js
+++ src/components/HeadingBar.js
@@ -12,35 +12,35 @@ import {logout, isAuthenticated} from '../auth/index'
 const HeadingBar = ({ history }) => {
     return (
     <div>
-    {/*<header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+    {/*<header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
         
-        <h1><img src={logo} width="30" className=""/><span class="px-3">swap street</span></h1>
+        <h1><img src={logo} width="30" className=""/><span className="px-3">swap street</span></h1>
       </a>
 
-      <ul class="nav nav-pills">
-        <li class="nav-item"><Link to="/" class="nav-link active" aria-current="page">Home</Link></li>
-        <li class="nav-item"><Link to="/" class="nav-link">About</Link></li>
-        {isAuthenticated() ? (<><li class="nav-item"><Link to="/dashboard" className="nav-link">Dashboard</Link></li>
-        <li class="nav-item"><Link to="/" className="nav-link">Logout</Link></li></>) :
-        (<li class="nav-item"> <Link to="/login" className="nav-link">Login/Sign Up</Link></li>)}
+      <ul className="nav nav-pills">
+        <li className="nav-item"><Link to="/" className="nav-link active" aria-current="page">Home</Link></li>
+        <li className="nav-item"><Link to="/" className="nav-link">About</Link></li>
+        {isAuthenticated() ? (<><li className="nav-item"><Link to="/dashboard" className="nav-link">Dashboard</Link></li>
+        <li className="nav-item"><Link to="/" className="nav-link">Logout</Link></li></>) :
+        (<li className="nav-item"> <Link to="/login" className="nav-link">Login/Sign Up</Link></li>)}
         
       </ul>
 </header>*/}
-<nav class="navbar navbar-expand-lg navbar-light rounded" aria-label="Eleventh navbar example">
-      <div class="container-fluid">
-        <Link to="/" className="navbar-brand"><h1><img src={logo} width="30" className=""/><span class="px-3">swap street</span></h1></Link>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+<nav className="navbar navbar-expand-lg navbar-light rounded" aria-label="Eleventh navbar example">
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand"><h1><img src={logo} width="30" className=""/><span className="px-3">swap street</span></h1></Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarsExample09">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item"><Link to="/" class="nav-link" aria-current="page">Home</Link></li>
-        <li class="nav-item"><Link to="/" class="nav-link">About</Link></li>
-        {isAuthenticated() ? (<><li class="nav-item"><Link to="/dashboard" className="nav-link">Dashboard</Link></li>
-        <li class="nav-item"><Link to="/" className="nav-link">Logout</Link></li></>) :
-        (<li class="nav-item"> <Link to="/login" className="nav-link">Login/Sign Up</Link></li>)}
+        <div className="collapse navbar-collapse" id="navbarsExample09">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item"><Link to="/" className="nav-link" aria-current="page">Home</Link></li>
+        <li className="nav-item"><Link to="/" className="nav-link">About</Link></li>
+        {isAuthenticated() ? (<><li className="nav-item"><Link to="/dashboard" className="nav-link">Dashboard</Link></li>
+        <li className="nav-item"><Link to="/" className="nav-link">Logout</Link></li></>) :
+        (<li className="nav-item"> <Link to="/login" className="nav-link">Login/Sign Up</Link></li>)}
           </ul>
         </div>
       </div>
