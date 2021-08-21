131f66116f3b3184e73beb6d4d5ed8c1515b6a82
rlycano07 <baral.rushan@gmail.com>
2021-08-21 23:12:08 +1000

    feat: about page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 const Footer = () => (
-  <div className="footer">
-    <p><b>&copy; WeRware Pty Ltd 2020-2021&nbsp;&nbsp;</b>
-      <i>SwapStreet is a software engineering team project in Comp4050 S2 2021, Macqurie University
-    </i></p>
-  </div>
-  
+  <section>
+    <div className="footer">
+      <p><b>&copy; WeRware Pty Ltd 2020-2021&nbsp;&nbsp;</b>
+        <i>SwapStreet is a software engineering team project in Comp4050 S2 2021, Macqurie University
+      </i></p>
+    </div>
+  </section>
 );
 
 export default Footer;
