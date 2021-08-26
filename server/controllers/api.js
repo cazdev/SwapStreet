1e53d0d011cf83c1a882fde3e7a4c4c316f30cb7
Joshua Archer <joshua.archer@students.mq.edu.au>
2021-08-26 14:14:37 +1000

    fixed bug

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -59,12 +59,13 @@ apiRouter.put('/api/users/:id', async (request, response) => {
   if (!user) {
     return response.status(401).json({ error: 'user does not exist' })
   }
+  
   const body = request.body
   const pWord = request.body.password
   const encryptedPass = bcrypt.hashSync(pWord, 10)
   user.name = body.name ? body.name : (user.name ? user.name : ""), 
   user.email = body.email ? body.email : (user.email ? user.email : ""),
-  user.password = encryptedPass ? body.password : (user.password ? user.password : ""),
+  user.password = encryptedPass ? encryptedPass : (user.password ? user.password : ""),
   user.address = body.address ? body.address : (user.address ? user.address : ""),
   user.about = body.about ? body.about : ""
   user.coins = body.coins ? body.coins : (user.coins ? user.coins : 0)
