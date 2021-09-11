61241fac446b74eaa80e84a8dd7084757ed2a58f
JoshDeeNc <61564763+JoshDeeNc@users.noreply.github.com>
2021-09-11 13:27:12 +1000

    added map to favours homepage

---
+++ src/components/map/map.js
@@ -0,0 +1,47 @@
+import { useEffect } from 'react'
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
+
+import '../../../node_modules/leaflet/dist/leaflet.css'
+import './react-leaflet-geosearch.css'
+
+import SearchControl from './searchControl'
+import OpenStreetMapProvider from './openStreetMapProvider'
+
+
+
+const MapComp = () => {
+
+    const prov = OpenStreetMapProvider();
+    const GeoSearchControlElement = SearchControl;
+
+    return (
+        <MapContainer
+            center={[-33.77432, 151.11271]}
+            zoom={14}
+            zoomControl={true}
+        >
+            <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+            />
+            <GeoSearchControlElement
+                provider={prov}
+                showMarker={true}
+                showPopup={true}
+                popupFormat={({ query, result }) => result.label}
+                maxMarkers={3}
+                retainZoomLevel={false}
+                animateZoom={true}
+                autoClose={false}
+                searchLabel={"Enter address, please"}
+                keepResult={true}
+            />
+            <Marker position={[-33.77432, 151.11271]}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
+        </MapContainer>
+    )
+}
+export default MapComp;
