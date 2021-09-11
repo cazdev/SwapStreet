69a266b99e8667a5121f323a7d1be5b88a092a28
JoshDeeNc <61564763+JoshDeeNc@users.noreply.github.com>
2021-09-11 13:51:02 +1000

    fixed searching to australia only

--- src/components/map/openStreetMapProvider.js
+++ src/components/map/openStreetMapProvider.js
@@ -1,5 +1,10 @@
 import { OpenStreetMapProvider as LeafletOpenStreetMapProvider } from 'leaflet-geosearch'
 
-const OpenStreetMapProvider = () => new LeafletOpenStreetMapProvider()
+const OpenStreetMapProvider = () => new LeafletOpenStreetMapProvider({
+    params: {
+      countrycodes: 'au', // limit search results to the Netherlands
+      addressdetails: 1, // include additional address detail parts
+    },
+  })
 
 export default OpenStreetMapProvider
