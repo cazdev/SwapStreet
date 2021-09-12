9b9367ea0a45dbd26ec9e74b053a303fb5ce2b82
JoshDeeNc <61564763+JoshDeeNc@users.noreply.github.com>
2021-09-12 11:08:08 +1000

    added location changes to necessary files as well as a map on individual job pages

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,5 +1,6 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
+import { Link } from "react-router-dom";
 
 import '../../../node_modules/leaflet/dist/leaflet.css'
 import './react-leaflet-geosearch.css'
@@ -9,16 +10,34 @@ import OpenStreetMapProvider from './openStreetMapProvider'
 
 
 
-const MapComp = () => {
+const MapComp = (props) => {
+
+    const [jobList, setJobList] = useState([])
 
     const prov = OpenStreetMapProvider();
     const GeoSearchControlElement = SearchControl;
 
+    useEffect(() => {
+        if(props.jobList) {
+            setJobList(props.jobList)
+            console.log(props.jobList)
+        }
+    }, [props.jobList])
+
+    function SetViewOnClick({ coords }) {
+        const map = useMap();
+        if (coords.length > 0) {
+            map.setView(coords, map.getZoom());
+        } else {
+            map.setView([-33.77432, 151.11271], 10);
+        }
+        return null;
+    }
+
     return (
         <MapContainer
-            center={[-33.77432, 151.11271]}
-            zoom={14}
-            zoomControl={true}
+            center={jobList.length > 0 ? [jobList[0].location.y, jobList[0].location.x] :[-33.77432, 151.11271]}
+            zoom={15}
         >
             <TileLayer
                 url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
@@ -28,18 +47,28 @@ const MapComp = () => {
                 showMarker={true}
                 showPopup={true}
                 popupFormat={({ query, result }) => result.label}
-                maxMarkers={3}
+                maxMarkers={1}
                 retainZoomLevel={false}
                 animateZoom={true}
                 autoClose={false}
                 searchLabel={"Enter address, please"}
                 keepResult={true}
             />
-            <Marker position={[-33.77432, 151.11271]}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
+            {jobList.map((item,index) => {
+            if(jobList.length === 1) {
+                return item.location.x !== 100000 && (<Marker position={[item.location.y, item.location.x]}>
+                    <Popup>
+                        <Link to={`/job/${item._id}`}>{item.title}</Link>
+                    </Popup>
+                </Marker>)}
+            else {
+                return item.location.x !== 100000 && item.status === 0 && (<Marker position={[item.location.y, item.location.x]}>
+                    <Popup>
+                        <Link to={`/job/${item._id}`}>{item.title}</Link>
+                    </Popup>
+                </Marker>)
+            }})}
+            <SetViewOnClick coords={jobList.length === 1 && jobList[0].location.x !== 100000 ? [jobList[0].location.y, jobList[0].location.x] : []} />
         </MapContainer>
     )
 }
