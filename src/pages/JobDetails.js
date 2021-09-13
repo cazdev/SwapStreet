2944fc3f9a356351a5f55840ea6324993951470e
JoshDeeNc <61564763+JoshDeeNc@users.noreply.github.com>
2021-09-13 19:38:57 +1000

    added radius for map

--- src/pages/JobDetails.js
+++ src/pages/JobDetails.js
@@ -29,13 +29,13 @@ const JobDetails = () => {
   }, [])
 
   const buyProvideJob = async (event) => {
-    if(!isAuthenticated()) {
+    if (!isAuthenticated()) {
       history.push("/login")
     } else {
-      const submitted = job.providerUserId === "" ? (await updateJob({...job, providerUserId: userProf._id, status: 2}).catch((error) => {
+      const submitted = job.providerUserId === "" ? (await updateJob({ ...job, providerUserId: userProf._id, status: 2 }).catch((error) => {
         console.log(error.response.data.error)
         alert(error.response.data.error);
-      })) : (await updateJob({...job, clientUserId: userProf._id, status: 2}).catch((error) => {
+      })) : (await updateJob({ ...job, clientUserId: userProf._id, status: 2 }).catch((error) => {
         console.log(error.response.data.error)
         alert(error.response.data.error);
       }))
@@ -45,11 +45,11 @@ const JobDetails = () => {
   }
 
   const addCom = async () => {
-    const submitted = await addComment({comment: newComment, providerUserId: job.providerUserId, clientUserId: userProf._id }).catch((error) => {
+    const submitted = await addComment({ comment: newComment, providerUserId: job.providerUserId, clientUserId: userProf._id }).catch((error) => {
       console.log(error.response.data.error)
       alert(error.response.data.error);
     })
-    if(submitted){
+    if (submitted) {
       setUserComments([...userComments, submitted])
       const tarea = document.getElementById("txtcom")
       tarea.disabled = true
@@ -57,13 +57,13 @@ const JobDetails = () => {
   }
 
   const swapFav = async () => {
-    if(!isAuthenticated()) {
+    if (!isAuthenticated()) {
       history.push("/login")
     } else {
-      const submitted = job.providerUserId === "" ? (await updateJob({...job, swapReqUserId: userProf._id, status: 1}).catch((error) => {
+      const submitted = job.providerUserId === "" ? (await updateJob({ ...job, swapReqUserId: userProf._id, status: 1 }).catch((error) => {
         console.log(error.response.data.error)
         alert(error.response.data.error);
-      })) : (await updateJob({...job, swapReqUserId: userProf._id, status: 1}).catch((error) => {
+      })) : (await updateJob({ ...job, swapReqUserId: userProf._id, status: 1 }).catch((error) => {
         console.log(error.response.data.error)
         alert(error.response.data.error);
       }))
@@ -75,58 +75,64 @@ const JobDetails = () => {
   console.log(job)
 
   return (<>
-    <div class="row flex-lg-row-reverse g-5 py-5">
-      <div class="col-10 col-sm-8 col-lg-6 job-image">
-          <div className="map-container">
-          {job.location && <MapComp jobList={[job]}/>}
-          </div>
-      </div>
+    <div class="row g-5 py-5">
       <div class="col-md-6">
-        <h1 class="lh-1 mb-3 txt-blue">{job.title}</h1>
-        <p class="lead">{job.description}</p>
-        {job.location && <p class="lead">{typeof job.location === "string" ? job.location: job.location.label}</p>}
-        {job.skill !== undefined && job.skill.length > 0 && (<><p class="lead">Skills needed</p>
-        <ul>
-          {job.skill && job.skill.map(skill => (
-            <li>{skill}</li>
-          ))}
-        </ul>
-        </>)}
-        {!userProf || (userProf && job.status === 0 && userProf._id !== job.clientUserId && userProf._id !== job.providerUserId) ? 
-        (<div class="d-grid gap-2 d-md-flex justify-content-md-start">
-          <button onClick={(e) => buyProvideJob(e)} type="button" class="btn btn-primary btn-sm px-4 me-md-2">{job.price} Swapstreet Coins</button>
-          <button onClick={(e) => swapFav()} type="button" class="btn btn-outline-secondary btn-sm px-4">Swap Services</button>
-        </div>) :(<></>)}
-      </div>
-    </div>
+        <div class="row">
+          <div class="col-md-12">
 
-    <div class="row py-5">
-    <div class="col-md-6">
-        <div class="p-3 b border">
-          <h2> About {user.name}</h2>
-          <ul className="mt-3 mb-4">
-              <li>{user.email}</li>
-              {user.address && <li>{typeof user.address === "string" ? user.address: user.address.label}</li>}
-              {user.about !== "" && <li>{user.about}</li>}
-          </ul>
-            {userComments && (<><h2>Reviews</h2>
-            <ul className="mt-3 mb-4">
-              {userComments.map(com => <li key={com._id}>{com.comment}</li>)}
-            </ul>
-          </>)}
-          {isAuthenticated() && job.clientUserId === "" && userProf._id !== job.clientUserId && userProf._id !== job.providerUserId && (
-          <form>
-          <div className="form-group">
-                <label className="text-muted">Comment on {user.name}'s services</label>
-                <textarea rows="2" onChange={(e) => setNewComment(e.target.value)} className="form-control" value={newComment} id="txtcom"/>
+            <h1 class="lh-1 mb-3 txt-blue">{job.title}</h1>
+            <p class="lead">{job.description}</p>
+            {job.location && <p class="lead">{typeof job.location === "string" ? job.location : job.location.label}</p>}
+            {job.skill !== undefined && job.skill.length > 0 && (<><p class="lead">Skills needed</p>
+              <ul>
+                {job.skill && job.skill.map(skill => (
+                  <li>{skill}</li>
+                ))}
+              </ul>
+            </>)}
+            {!userProf || (userProf && job.status === 0 && userProf._id !== job.clientUserId && userProf._id !== job.providerUserId) ?
+              (<div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                <button onClick={(e) => buyProvideJob(e)} type="button" class="btn btn-primary btn-sm px-4 me-md-2">{job.price} Swapstreet Coins</button>
+                <button onClick={(e) => swapFav()} type="button" class="btn btn-outline-secondary btn-sm px-4">Swap Services</button>
+              </div>) : (<></>)}
+          </div>
+
+          <div class="col-md-12">
+            <div class="row py-5">
+              <div class="col-md-12">
+                <hr />
+                <div class="p-3 b ">
+                  <h2> About {user.name}</h2>
+                  <ul className="mt-3 mb-4">
+                    <li>{user.email}</li>
+                    {user.address && <li>{typeof user.address === "string" ? user.address : user.address.label}</li>}
+                    {user.about !== "" && <li>{user.about}</li>}
+                  </ul>
+                  {userComments && (<><h2>Reviews</h2>
+                    <ul className="mt-3 mb-4">
+                      {userComments.map(com => <li key={com._id}>{com.comment}</li>)}
+                    </ul>
+                  </>)}
+                  {isAuthenticated() && job.clientUserId === "" && userProf._id !== job.clientUserId && userProf._id !== job.providerUserId && (
+                    <form>
+                      <div className="form-group">
+                        <label className="text-muted">Comment on {user.name}'s services</label>
+                        <textarea rows="2" onChange={(e) => setNewComment(e.target.value)} className="form-control" value={newComment} id="txtcom" />
+                      </div>
+                      <button onClick={addCom} type="button" class="btn btn-primary btn-sm px-4 mt-2 me-md-2">Add Comment</button>
+                    </form>)}
+                </div>
+              </div>
             </div>
-          <button onClick={addCom} type="button" class="btn btn-primary btn-sm px-4 mt-2 me-md-2">Add Comment</button>
-          </form>)}
+          </div>
         </div>
       </div>
+      <div class="col-sm-6 job-image">
+        <div className="map-container">
+          {job.location && <MapComp jobList={[job]} />}
+        </div>
       </div>
-
-
+    </div>
     <div class="row ">
       <div class="col">
         <h2 class="txt-blue">Similar Services</h2>
