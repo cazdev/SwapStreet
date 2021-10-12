3207878770de3ac33881899c252036875015e072
Rushan Baral <r.lycano07@192-168-1-102.tpgi.com.au>
2021-10-12 13:49:28 +1100

    doc: T&C for users added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,24 @@
 import React from "react";
 
-const Footer = () => (
-  <section>
+const Footer = () => {
+
+  return(
     <div className="footer">
-      <p><b>&copy; WeRware Pty Ltd 2020-2021&nbsp;&nbsp;</b>
-        <i>SwapStreet is a software engineering team project in Comp4050 S2 2021, Macqurie University
-      </i></p>
+      <p>
+        <b>
+          &copy; WeRware Pty Ltd 2020-2021&nbsp;&nbsp;
+        </b>
+        <i>
+          SwapStreet is a software engineering team project in Comp4050 S2 2021, Macqurie University
+        </i>
+      </p>
+      <ul>
+        <li>
+          {/* <Link to="/tandc">Terms and conditions.</Link> */}
+        </li>
+      </ul>
     </div>
-  </section>
-);
+  );
+}
 
 export default Footer;
